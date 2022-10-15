b8b7f1e3278639890c45f2fae136cd377dfebdcc
Moozaheed <57921369+Barbarian96@users.noreply.github.com>
2022-10-15 22:51:18 +0600

    Last

--- C_Phase_Shift.cpp
+++ C_Phase_Shift.cpp
@@ -1,78 +1,126 @@
-/// :3
-#pragma GCC optimize("O3")
-#pragma GCC optimize ("unroll-loops")
-#pragma GCC target("avx,avx2,fma")
-using namespace std;
-
-#include "bits/stdc++.h"
-
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
 #define pb push_back
-#define F first
-#define S second
-#define f(i, a, b)  for(int i = a; i < b; i++)
-#define all(a)  a.begin(),a.end()
-#define rall(a) a.rbegin(),a.rend()
-#define sz(x) (int)(x).size()
-#define mp(x,y) make_pair(x,y)
-#define popCnt(x) (__builtin_popcountll(x))
-#define int ll
+#define pob pop_back
+#define in insert
+#define vi vector<ll int>
+#define vs vector<string>
+#define pii pair<ll int,ll int>
+#define psi pair<string,ll int>
+#define pis pair<ll int,string>
+#define mii map<ll int,ll int>
+#define mib map<ll int,bool>
+#define msi map<string,ll int>
+#define mis map<ll int,string>
+#define S string
+#define fi first
+#define se second
+#define sp ' '
+#define dl "\n"
+#define all(data) data.begin(),data.end()
+#define b() begin()
+#define e() end()
+#define cY cout<<"YES\n"
+#define cN cout<<"NO\n"
+#define cy cout<<"Yes\n"
+#define cn cout<<"No\n"
+#define setp(n) setprecision(n)
+#define mem(arr,val) memset(arr,val,sizeof(arr));
+#define debug(x) cerr<<x<<dl;
+#define here fprintf(stderr, "====I am Here====\n");
+#define FOR(data) for(auto it=data.begin();it!=data.end();it++)
+#define Boost ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define maxx 100005
+#define EPS 1e-9
+using namespace std;
+const double PI = acos(-1);
+ll int hp = 1e9+7;
 
-using ll = long long;
-using ii = pair<int, int>;
-using ull = unsigned long long;
+map<char,char>mp,mp1;
+bool check(char ch1,char ch2)
+{
+    char ch;
+    while(mp[ch1]!=ch)
+    {
+        ch1=mp[ch1];
+        if(ch1==ch2)return false;
+    }
+    return true;
+}
 
-const int N = 2e6+5, LG = 18, MOD = (119 << 23) + 1;
-const long double PI = acos(-1);
-const long double EPS = 1e-7;
-const int dx[] = {1,1,0,-1,-1,-1,0,1};
-const int dy[] = {0,1,1,1,0,-1,-1,-1};
+void Boom()
+{
+    //Let's Move
 
-int nxt[26];
-int prv[26];
-void doWork() {
-
-    memset(nxt,-1, sizeof nxt);
-    memset(prv,-1, sizeof prv);
-    string ans = "";
-    int n;  cin >> n;
-    string s;   cin >> s;
-    f(i,0,n) {
-        if(nxt[s[i]-'a'] == -1) {
-            bool ok = false;
-            bitset<26> sameChain;
-            int cur = s[i]-'a';
-            sameChain[cur]=1;
-            while(prv[cur] != -1) {
-                cur = prv[cur];
-                sameChain[cur]=1;
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        string ans="";
+        
+        for(int i=0;i<n;i++)
+        {
+            char ch=s[i];
+            if(mp[ch]!=0)
+            {
+                ans+=mp[ch];
             }
-            for(int j = 0; j < 26; j++)if(!sameChain[j]){
-                if(prv[j] == -1) {
-                    prv[j] = s[i] - 'a';
-                    nxt[s[i]-'a'] = j;
-                    ok=true;
-                    break;
+            else{
+                int f=0;
+                for(char ch1='a';ch1<='z';ch1++)
+                {
+                    if(ch!=ch1 && mp1[ch1]==0 && check(ch1,ch))
+                    {
+                        mp1[ch1]=ch;
+                        mp[ch]=ch1;
+                        ans+=ch1;
+                        f=1;
+                        break;
+                    }
+                }
+                if(f==0)
+                {
+                    for(char ch1='a'; ch1<='z'; ch1++)
+                    {
+                        if(mp1[ch1]==0)
+                        {
+                            mp[ch]=ch1;
+                            ans+=ch1;
+                            break;
+                        }
+                    }
                 }
             }
-            if(!ok)nxt[s[i]-'a']=cur;
         }
-        ans.push_back(nxt[s[i]-'a']+'a');
-    }
+        cout<<ans<<endl;
 
-    cout << ans << '\n';
+
+        mp.clear();
+        mp1.clear();
+
+
+    
+
+    
+
+
 
 
 }
 
-int32_t main() {
-#ifdef ONLINE_JUDGE
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-#endif // ONLINE_JUDGE
-    int t = 1;
-    cin >> t;
-    while (t--) {
-        doWork();
+int main()
+{
+    Boost;
+
+    int t=1;       cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        //cout<<"Case "<<i<<" : ";
+        Boom();
     }
+
     return 0;
+
 }
