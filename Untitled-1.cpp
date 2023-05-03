8b42e649f1ff53c0ac270c71a40658987a595702
Moozaheed <57921369+Moozaheed@users.noreply.github.com>
2023-05-03 20:19:18 +0600

    ok

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -2,51 +2,20 @@
 using namespace std;
 int main()
 {
-    int n,q;
-    cin>>n>>q;
 
-    int arr[n];
+  //Digit Count
+  int n;
+  cin>>n;
 
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
+  int cnt=0;
+ 
+  while(n!=0)
+  {
+    cnt++;
+    n/=10;
+  }
 
-    sort(arr,arr+n);
-for(int k=0;k<q;k++)
-{
-    int p;
-    cin>>p;
-    int l=0,r=n-1,ans=-1;
-
-    while(l<=r)
-    {
-        int mid=(l+r)/2;
-
-        if(arr[mid]==p)
-        {
-            ans=mid;
-            break;
-        }
-        else if(arr[mid]<p)
-        {
-            l=mid+1;
-        }
-        else
-        {
-            r=mid-1;
-        }
-    }
-    if(ans==-1)
-    {
-        cout<<"not found"<<endl;
-    }
-    else
-    {
-        cout<<"found"<<endl;
-    }
-
-
-}
+  cout<<cnt<<endl;
+ 
 
 }
